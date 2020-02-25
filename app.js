f665131270b5a3ff3dffac92763226ce276b0b4a
Wilson <p780729@hotmail.com>
2020-02-26 01:09:19 +0800

    Use eslint for JS standard

--- app.js
+++ app.js
@@ -5,47 +5,41 @@ const bodyParser = require('body-parser')
 const port = 3000
 const nonsenseGenerator = require('./nonsense_generator')
 
-//setting template engine
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
-//註冊樣板 ifEquals 方法
+// 註冊樣板 ifEquals 方法
 app.engine('handlebars', exphbs({
-    helpers: {
-        ifEquals : function(arg1, arg2, options){
-            console.log('arg1', arg1)
-            console.log('arg2', arg2)
-            console.log('this', this) //
-            console.log('options', options) //
-            console.log('options.fn', options.fn(this)) //
-            if(arg1 === arg2) return options.fn(this)
-        }
+  helpers: {
+    ifEquals: function (arg1, arg2, options) {
+      console.log('arg1', arg1)
+      console.log('arg2', arg2)
+      console.log('this', this) //
+      console.log('options', options) //
+      console.log('options.fn', options.fn(this)) //
+      if (arg1 === arg2) return options.fn(this)
     }
+  }
 }))
 app.set('view engine', 'handlebars')
 
-//setting handlebars helper
-// exphbs.registerHelper('ifEquals', function(arg1, arg2, options) {
-//     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-// });
-
-
-//setting bodyParser
+// setting bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 })
 
-app.post('/', (req, res)=> {
-    let jobs = req.body.jobs
+app.post('/', (req, res) => {
+  const jobs = req.body.jobs
 
-    console.log('jobs', jobs)
-    console.log('nonsecse', nonsenseGenerator(jobs))
+  console.log('jobs', jobs)
+  console.log('nonsecse', nonsenseGenerator(jobs))
 
-    let sentance = nonsenseGenerator(jobs)
-    res.render('index', {jobs, sentance})
+  const sentance = nonsenseGenerator(jobs)
+  res.render('index', { jobs, sentance })
 })
 
 app.listen(port, () => {
-    console.log(`nonsecse_generator server is running on port ${port}`)
+  console.log(`nonsecse_generator server is running on port ${port}`)
 })
--- nonsense_generator.js
+++ nonsense_generator.js
@@ -1,35 +1,34 @@
-function nonsense_generator(jobs){
+/* eslint-disable space-before-function-paren */
+// eslint-disable-next-line camelcase
+function nonsense_generator(jobs) {
+  // 隨機取出一個 index
+  function randomIndex(arr) {
+    const len = arr.length
+    return Math.floor(Math.random() * len)
+  }
 
-    //隨機取出一個 index
-    function randomIndex(arr){
-        const len = arr.length
-        return Math.floor(Math.random() * len)
-    }
+  const task = {
+    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
+    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+  }
+  const phrase = ['很簡單', '很容易', '很快', '很正常']
+  let sentance = '身為一個'
+  if (jobs === 'engineer') {
+    sentance += '工程師，' + task.engineer[randomIndex(task.engineer)]
+  } else if (jobs === 'designer') {
+    sentance += '設計師，' + task.designer[randomIndex(task.designer)]
+  } else if (jobs === 'founder') {
+    sentance += '創業家，' + task.entrepreneur[randomIndex(task.entrepreneur)]
+  } else {
+    return '請先選擇職業'
+  }
 
-    const task = {
-        engineer: ['加個按鈕','加新功能','切個版', '改一點 code'],
-        designer: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
-        entrepreneur: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
-    }
-    const phrase = ['很簡單','很容易','很快','很正常']
-    let sentance='身為一個'
-    if(jobs === 'engineer'){
-        sentance += '工程師，' + task.engineer[randomIndex(task.engineer)]
-    
-    }else if(jobs === 'designer'){
-        sentance += '設計師，' + task.designer[randomIndex(task.designer)]
-    
-    }else if(jobs === 'founder'){
-        sentance += '創業家，' + task.entrepreneur[randomIndex(task.entrepreneur)]
-    
-    }else{
-        return '請先選擇職業'
-    }
-
-    sentance += '，' + phrase[randomIndex(phrase)] + '吧！'
-    return sentance
+  sentance += '，' + phrase[randomIndex(phrase)] + '吧！'
+  return sentance
 }
 
+// eslint-disable-next-line camelcase
 module.exports = nonsense_generator
 
 // let jobs = 'designer'
